f0e534dc7884ea2d60ff0127dbb125fb31876567
Charlemagne <lemuelpuglisi001@gmail.com>
2021-09-08 00:42:44 +0200

    add style

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,64 +1,68 @@
 <template>
+    <Sidebar v-model:visible="sidebarFlag"> Content </Sidebar>
+    <Button icon="pi pi-arrow-right" @click="sidebarFlag = !sidebarFlag" />
+    <h1>porcodiooo</h1>
 
-  <div class="p-grid">
-    <div class="p-col-4">
-      <connection-handler/>
+    <div class="p-grid">
+        <div class="p-col-4">
+            <connection-handler />
+        </div>
+        <div class="p-col-8">
+            <connection-dashboard />
+        </div>
     </div>
-    <div class="p-col-8">
-      <connection-dashboard/>
-    </div>
-  </div>
 
-  <hr>
-  <button @click="debug">debug call</button>
-  <p v-html="debugResult"></p>
-
-
+    <hr />
+    <button @click="debug">debug call</button>
+    <p v-html="debugResult"></p>
 </template>
 
 <script>
+import Sidebar from "primevue/sidebar/sfc";
+import Button from "primevue/button/sfc";
 
-import ConnectionHandler from './ConnectionHandler.vue';
-import ConnectionDashboard from './ConnectionDashboard.vue';
-import KannelConnection from '../class/KannelConnection';
-import CrossOriginHttp from '../class/CrossOriginHttp';
+import ConnectionHandler from "./ConnectionHandler.vue";
+import ConnectionDashboard from "./ConnectionDashboard.vue";
+import KannelConnection from "../class/KannelConnection";
+import CrossOriginHttp from "../class/CrossOriginHttp";
 
 export default {
-  name: 'App',
-  components: {
-    ConnectionHandler, 
-    ConnectionDashboard
-  }, 
-  data() {
-    return {
-      debugResult: '<h1>OK</h1>'
-    }
-  },
-  methods: {
-    debug() {
-      const proxy = new CrossOriginHttp('http://localhost:8181'); 
-      const connection = new KannelConnection(proxy, 'localhost', 'bar'); 
-      connection.connect(); 
-      console.log(connection.info)
-    }
-  },
-  created() {
-    setInterval(() => {
-      this.$store.dispatch('pingAllConnections')
-    }, 2000); 
-  }
-}
+    name: "App",
+    components: {
+        ConnectionHandler,
+        ConnectionDashboard,
+        Sidebar,
+        Button,
+    },
+    data() {
+        return {
+            debugResult: "<h1>OK</h1>",
+            sidebarFlag: true,
+        };
+    },
+    methods: {
+        debug() {
+            const proxy = new CrossOriginHttp("http://localhost:8181");
+            const connection = new KannelConnection(proxy, "localhost", "bar");
+            connection.connect();
+            console.log(connection.info);
+        },
+    },
+    created() {
+        setInterval(() => {
+            this.$store.dispatch("pingAllConnections");
+        }, 2000);
+    },
+};
 </script>
 
 <style scoped>
-
-  table {
+table {
     text-align: left;
-  }
+}
 
-  #app {
-    padding: 20px; 
+#app {
+    padding: 20px;
     width: 100%;
-  }
-
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import PrimeVue from 'primevue/config';
 
 import '@/assets/css/main.css'; 
 import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/bootstrap4-dark-purple/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App); 
 
