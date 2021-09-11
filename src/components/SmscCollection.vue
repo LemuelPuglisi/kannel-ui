972d00454b50a0575699d7f8ed3e1ba27a515581
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2021-09-11 14:33:29 +0200

    add smsc boxes

--- src/components/SmscCollection.vue
+++ src/components/SmscCollection.vue
@@ -1,17 +1,59 @@
 <template>
   <Fieldset class="p-col" legend="SMSC List">
-      
+    <div class="fix-w-parent">
+
+      <div class="p-d-flex p-flex-wrap p-jc-center">
+        <smsc-item v-for="(smsc, index) in smscs" :key="index" :smsc="smsc"/>
+      </div>
+
+    </div>
   </Fieldset>
 </template>
 
 <script>
+import SmscItem from "./SmscItem.vue"; 
 import Fieldset from "primevue/fieldset/sfc";
 
 export default {
   name: "smsc-collection",
   props: ["connection"],
   components: {
+    SmscItem, 
     Fieldset,
+  },
+  computed: {
+    smscs() {
+      const smscList = this.connection.info.smscs.smsc;
+      const tmp = smscList.map((smsc) => {
+        const statusTokens = smsc.status.value.split(" ");
+        const status = statusTokens[0];
+        const uptime = statusTokens[1];
+
+        const nameTokens = smsc.name.value.split(":");
+        const [protocol, host, port, user] = nameTokens;
+
+        return {
+          id: smsc.id.value,
+          protocol: protocol,
+          host: host,
+          port: port,
+          user: user,
+          adminId: smsc["admin-id"].value,
+          status: status,
+          uptime: uptime,
+          failed: smsc.failed.value,
+          queued: smsc.queued.value,
+          sentSms: smsc.sms.sent.value,
+          receivedSms: smsc.sms.received.value,
+          sentDlr: smsc.dlr.sent.value,
+          receivedDlr: smsc.dlr.received.value,
+        };
+      });
+      for (let i = 0; i < 10; i++) {
+        tmp.push(tmp[0]);
+      }
+      return tmp;
+    },
   },
 };
 </script>
---
+++ src/components/SmscItem.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="smsc-outer-box">
+    <div class="smsc-box" />
+    <div class="smsc-content">
+      <div style="text-align: center">
+        <div class="p-m-3">
+            <Tag :value="smsc.status" :severity="statusSeverity"/>
+        </div>
+        <b>{{ smsc.id }}</b> <br />
+        <small> {{ smsc.host }} </small> <br>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tag from 'primevue/tag';
+
+const statusColorMap = new Map([
+    ['online', 'success'], 
+    ['re-connecting', 'warning'], 
+    ['dead', 'danger']
+])
+
+export default {
+    name: 'smsc-item', 
+    props: ['smsc'], 
+    components: {
+        Tag, 
+    }, 
+    computed: {
+        statusSeverity() {
+            return statusColorMap.get(this.smsc.status); 
+        }
+    }
+}
+</script>
+
+<style scoped>
+*,
+*:before,
+*:after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.smsc-outer-box {
+  position: relative;
+  width: 240px;
+  height: 220px;
+}
+
+.smsc-box {
+  height: 150px;
+  width: 200px;
+  position: absolute;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  box-shadow: 0 20px 35px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  overflow: hidden;
+  background-color: #20262e;
+}
+.smsc-box:before {
+  content: "";
+  background-image: conic-gradient(#c298d8 20deg, transparent 120deg);
+  height: 150%;
+  width: 150%;
+  position: absolute;
+  left: -25%;
+  top: -25%;
+  animation: rotate 2s infinite linear;
+}
+@keyframes rotate {
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+.smsc-box:after {
+  content: "";
+  height: 94%;
+  width: 94%;
+  position: absolute;
+  background-color: #20262e;
+  border-radius: 5px;
+  top: 3%;
+  left: 3%;
+  color: #04b0ee;
+  display: grid;
+  place-items: center;
+  font-size: 20px;
+  letter-spacing: 6px;
+}
+
+.smsc-content {
+  position: absolute;
+  height: 150px;
+  width: 200px;
+  top:  16%;
+  left: 10%; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  cursor: pointer;
+  border-radius: 5px;  
+  transition: .5s;
+}
+
+.smsc-content:hover {
+  background-color: rgba(2, 2, 2, 0.5);
+}
+
+</style>
