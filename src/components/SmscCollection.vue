b71390259b0d400128ec7bf2a07f16148aa04bfe
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2021-09-11 15:22:00 +0200

    add smsc modal

--- src/components/SmscCollection.vue
+++ src/components/SmscCollection.vue
@@ -1,25 +1,52 @@
 <template>
   <Fieldset class="p-col" legend="SMSC List">
     <div class="fix-w-parent">
-
       <div class="p-d-flex p-flex-wrap p-jc-center">
-        <smsc-item v-for="(smsc, index) in smscs" :key="index" :smsc="smsc"/>
+        <smsc-item
+          v-for="(smsc, index) in smscs"
+          :key="index"
+          :smsc="smsc"
+          @click="selectSmsc(smsc)"
+        />
       </div>
-
     </div>
   </Fieldset>
+  <!-- smsc modal -->
+  <smsc-modal :smsc="selectedSmsc" @close-smsc-modal="removeSelectedSmsc" />
 </template>
 
 <script>
-import SmscItem from "./SmscItem.vue"; 
+import SmscItem from "./SmscItem.vue";
+import SmscModal from "./SmscModal.vue";
 import Fieldset from "primevue/fieldset/sfc";
+
+/**
+ * SMSC Selection FLOW 
+ * -----------------------------------------------------------------------------------
+ * SMSC is selected from the list using the "click" event on SmscItem component.
+ * The smsc is binded to selectedSmsc variable, that is passed as a prop to the 
+ * SmscModal component. The modal component watches the passed smsc, and when it 
+ * isn't null, then it displays the modal, updating a variable called displayModal. 
+ * This is not done with computed props because the inner Dialog component needs to 
+ * set this flag as false when the user closes the modal. The displayModel variable
+ * is watched by SmscModal and, when it's set to false, the component emit a 
+ * close-smsc-modal event. This event is catched here and handled with the 
+ * removeSelectedSmsc method. 
+ * -----------------------------------------------------------------------------------
+ */
 
 export default {
   name: "smsc-collection",
   props: ["connection"],
   components: {
-    SmscItem, 
+    SmscItem,
+    SmscModal,
     Fieldset,
+  },
+  data() {
+    return {
+      selectedSmsc: null,
+    };
   },
   computed: {
     smscs() {
@@ -53,6 +80,14 @@ export default {
         tmp.push(tmp[0]);
       }
       return tmp;
+    },
+  },
+  methods: {
+    selectSmsc(smsc) {
+      this.selectedSmsc = smsc;
+    },
+    removeSelectedSmsc() {
+      this.selectedSmsc = null;
     },
   },
 };
---
+++ src/components/SmscModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <Dialog
+    v-if="smsc"
+    v-model:visible="displayModal"
+    position="left"
+    class="p-shadow-24"
+    :breakpoints="{ '960px': '75vw', '640px': '100vw' }"
+    :style="{ width: '50vw' }"
+    :modal="true"
+  >
+    <template #header>
+      <h1>{{ smsc.id }}</h1>
+    </template>
+
+    <p>{{smsc}}</p>
+
+    <template #footer>
+      <!-- <Button label="No" icon="pi pi-times" class="p-button-text"/>
+            <Button label="Yes" icon="pi pi-check" autofocus /> -->
+    </template>
+  </Dialog>
+
+</template>
+
+<script>
+import Dialog from "primevue/dialog/sfc";
+
+export default {
+  name: "smsc-modal",
+  props: ["smsc"],
+  components: {
+    Dialog,
+  },
+  data() {
+    return {
+      displayModal: false,
+    };
+  },
+  watch: {
+      smsc: function (smsc) {
+          this.displayModal = smsc != null; 
+      }, 
+
+      displayModal: function (display) {
+        if (!display) {
+            this.$emit('close-smsc-modal')
+        }
+      }
+  }
+};
+</script>
